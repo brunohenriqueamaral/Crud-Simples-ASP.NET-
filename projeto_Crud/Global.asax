a840f22535711cbba989d09235a98935aa5d2302
Bruno Amaral <bruno_henrique_a@hotmail.com>
2019-11-05 00:48:05 -0300

    Primeiro commit projeto crud simples

---
+++ projeto_Crud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projeto_Crud.MvcApplication" Language="C#" %>
---
+++ projeto_Crud/Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace projeto_Crud.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CPF { get; set; }
+        public string Email { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Telefone { get; set; }
+        
+    }
+}
